6b59afa6d92266b229140cd76750fb2bf5bcd9f3
Anthony Fodor <anthony.fodor@gmail.com>
2017-01-08 10:54:37 -0500

    ant build file

--- src/cluster/RunCrossClassifiersCluster.java
+++ src/cluster/RunCrossClassifiersCluster.java
@@ -1,4 +1,8 @@
 package cluster;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.HashMap;
 import java.util.List;
 
 import kraken.RunAllClassifiers;
@@ -6,9 +10,35 @@ import projectDescriptors.AbstractProjectDescription;
 
 public class RunCrossClassifiersCluster
 {
+	public static final String SCRIPT_DIR = "/nobackup/afodor_research/clusterArff/crossScriptDir";
+	
+	private static final int NUMBER_JOBS= 100;
+	private static final int NUM_PERMUTATIONS = 1000;
+	
 	public static void main(String[] args) throws Exception
 	{
+		BufferedWriter allWriter = new BufferedWriter(new FileWriter(new File(
+			SCRIPT_DIR + File.separator + "runAll.sh"	)));
+		
+		HashMap<Integer, BufferedWriter> writerMap = 
+				new HashMap<Integer,BufferedWriter>();
+		
+		for( int x=0;  x < NUMBER_JOBS; x++)
+		{
+			File aFile = new File(SCRIPT_DIR + File.separator + "run_" + x + ".sh");
+			BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
+					aFile.getAbsolutePath())));
+			
+			writerMap.put(x, allWriter);
+			
+			allWriter.write("qsub -q \"copperhead\" " +   aFile.getAbsolutePath());
+			
+			writer.write("#PBS -l procs=8,mem=64GB\n");
+		}
+		
 		List<AbstractProjectDescription> projectList = RunAllClassifiers.getAllProjects();
+		
+		int index=0;
 		
 		for( int t =0; t < RunAllClassifiers.TAXA_ARRAY.length; t++)
 		{ 
@@ -19,6 +49,11 @@ public class RunCrossClassifiersCluster
 					{
 						
 					}
+			
+			index++;
+			
+			if (index == NUMBER_JOBS)
+				index =0;
 		}
 		
 	}
