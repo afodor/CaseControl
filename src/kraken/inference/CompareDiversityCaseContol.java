8b43cc8099c8eb13c2ba5dcb1a940bd315f2e1d3
Anthony Fodor <anthony.fodor@gmail.com>
2017-01-17 20:01:33 -0500

    diversity case/contol; finally all warnings removed..

--- src/kraken/inference/CompareDiversityCaseContol.java
+++ src/kraken/inference/CompareDiversityCaseContol.java
@@ -5,16 +5,20 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.List;
 
 import kraken.RunAllClassifiers;
 import projectDescriptors.AbstractProjectDescription;
+import projectDescriptors.AllButOne;
 import utils.ConfigReader;
 
 public class CompareDiversityCaseContol
 {
-	/*
 	public static void main(String[] args) throws Exception
 	{
+		List<AbstractProjectDescription> list = AllButOne.getLeaveOneOutBaseProjects();
+		list.addAll(AllButOne.getLeaveOneOutProjects() );
+		
 		for( int x=0; x < RunAllClassifiers.TAXA_ARRAY.length; x++)
 		{
 			String taxa = RunAllClassifiers.TAXA_ARRAY[x];
@@ -22,16 +26,12 @@ public class CompareDiversityCaseContol
 			BufferedWriter writer =new BufferedWriter(new FileWriter(new File(
 				ConfigReader.getMergedArffDir() + File.separator + "diversity_" + taxa +".txt"	)));
 			
-			writer.write("projectName\tclassificationScheme\tsampleName\tcaseContol\tshannonDiversity\tcompoundKey\n");
+			writer.write("projectName\tsampleName\tcaseContol\tshannonDiversity\tcompoundKey\n");
 			
 			for( AbstractProjectDescription apd : RunAllClassifiers.getAllProjects())
 			{
-				addDiveristy(apd.getLogNormalizedKrakenCounts(taxa), AbstractProjectDescription.KRAKEN,
-						apd, writer);
-				addDiveristy(apd.getLogNormalizedRDPCounts(taxa), AbstractProjectDescription.RDP, apd,
-						writer);
-				addDiveristy( apd.getLogNormalizedClosedRefQiimeCounts(taxa), AbstractProjectDescription.QIIME_CLOSED,
-							apd, writer	);
+				addDiveristy(apd.getNonLogFileKrakenCommonScale(taxa), apd, writer,
+						apd.getProjectName() + "_linear");
 				
 			}
 			
@@ -59,8 +59,8 @@ public class CompareDiversityCaseContol
 		return - shannon;
 	}
 	
-	private static void addDiveristy(String inFilePath, String classification, AbstractProjectDescription apd,
-			BufferedWriter writer ) throws Exception
+	private static void addDiveristy(String inFilePath, AbstractProjectDescription apd,
+			BufferedWriter writer, String key ) throws Exception
 	{
 		if( inFilePath != null)
 		{
@@ -88,11 +88,10 @@ public class CompareDiversityCaseContol
 					if( caseControl != null)
 					{
 						writer.write(apd.getProjectName() + "\t");
-						writer.write(classification + "\t");
-						writer.write(splits[0] + "\t");
+						writer.write("sample_" + splits[0] + "\t");
 						writer.write(caseControl + "\t");
 						writer.write(getShannonDiversity(splits) + "\t");
-						writer.write(apd.getProjectName() + "_" + classification + "_" + caseControl + "\n");
+						writer.write(key + "_" + caseControl +  "\n");
 					}
 				}
 				
@@ -102,5 +101,4 @@ public class CompareDiversityCaseContol
 
 		}
 	}
-		*/
 }
